e1beae6eb02560d9b6abf0281f18e14c5e332a36
Eduardo Dorantes <eduardo.dorantes.07@gmail.com>
2023-07-26 14:02:42 -0600

    test - github comments integration

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const core = require('@actions/core');
 const fetch = require('node-fetch');
+const { Octokit } = require("@octokit/rest");
+const github = require('@actions/github');
 
 async function run() {
   try {
@@ -38,6 +40,41 @@ async function run() {
         headers['x-moropo-api-key'] = moropoApiKey;
     }
 
+    // Initialize octokit
+    const octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN, // you should set this secret in your GitHub repo
+    });
+
+    // Get the context of the workflow run
+    const context = github.context;
+    let comment_id;
+
+    if (context.payload.pull_request) {
+      // It's a pull request
+      const pull_request_number = context.payload.pull_request.number;
+
+      const initialComment = await octokit.issues.createComment({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        issue_number: pull_request_number, // in the case of PRs, the issue number is the PR number
+        body: `Test run has been triggered with test run ID: ${testRunId}`,
+      });
+
+      comment_id = initialComment.data.id;
+    } else {
+      // It's a commit
+      const sha = context.sha;
+
+      const initialComment = await octokit.repos.createCommitComment({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        commit_sha: sha,
+        body: `Test run has been triggered with test run ID: ${testRunId}`,
+      });
+
+      comment_id = initialComment.data.id;
+    }
+
     const response = await fetch('https://dev.moropo.com/.netlify/functions/triggerTestRun', {
       method: 'POST',
       body: JSON.stringify(body),
@@ -65,6 +102,29 @@ async function run() {
         await new Promise(r => setTimeout(r, 15000));
       }
     } while (!testComplete);
+
+    // Update the comment depending on the test status
+    const message = `The test run has ${checkStatusData.status.toLowerCase()} with test run ID: ${testRunId}`;
+
+    if (context.payload.pull_request) {
+      // It's a pull request
+      const pull_request_number = context.payload.pull_request.number;
+
+      await octokit.issues.updateComment({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        comment_id: comment_id,
+        body: message,
+      });
+    } else {
+      // It's a commit
+      await octokit.repos.updateCommitComment({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        comment_id: comment_id,
+        body: message,
+      });
+    }
 
     // Exit the action with code 0 or 1 depending on the final status
     if (checkStatusData.status === 'FAILED') {
