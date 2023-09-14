7a16f5ce290406823d5f67b85410c6280191cf5d
Dec Burns <deaglanburns@msn.com>
2023-09-14 14:25:45 +0100

    remove build

--- index.ts
+++ index.ts
@@ -55,42 +55,6 @@ const buildMessageString = ({
 [View Results](${url})
 `;
 
-const uploadBuild = async (
-  url: URL,
-  apiKey: string,
-  buildPath: string
-): Promise<IBuildUploadResponse> => {
-  if (!existsSync(buildPath)) {
-    throw new Error('Build file not found');
-  }
-
-  const fileName = path.basename(buildPath);
-  const fileData = await readFile(buildPath);
-  const formData = new FormData();
-  formData.append('file', fileData, {
-    filename: fileName,
-    filepath: buildPath,
-  });
-
-  const buildUpload = await fetch(`${url}apps/builds`, {
-    method: 'POST',
-    body: formData,
-    headers: {
-      'X-App-Api-Key': apiKey,
-      'User-Agent': 'moropo-github-action',
-    },
-  });
-
-  const responseJson = await buildUpload.json();
-
-  if (!buildUpload.ok) {
-    throw new Error(`Failed to upload build: ${JSON.stringify(responseJson)}`);
-  }
-  console.info('Successfully uploaded build.');
-
-  return responseJson;
-};
-
 const run = async (): Promise<void> => {
   try {
     let expoReleaseChannel: string | null = getInput('expo_release_channel');
